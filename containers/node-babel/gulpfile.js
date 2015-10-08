d95038002b92477b69330a13e3a92519ff703f3c
jmyrland <j@peanuts.no>
2015-10-08 13:02:46 +0200

    Refactored gulp tasks & dependencies

--- containers/node-babel/gulpfile.js
+++ containers/node-babel/gulpfile.js
@@ -1,41 +1,29 @@
-
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var sequence = require('gulp-sequence');
-var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
 var forever = require('forever-monitor');
-var sourcemaps = require('gulp-sourcemaps');
+var sequence = require('gulp-sequence');
 
-var SRC_FILES = ['src/**/*.js'];
-var COMPILED_SRC_DIR = 'build/source';
-var ENTRY_PATH = '/app.js';
+var SRC_PATH = 'src';
+var RUN_COMMAND = 'npm run babel-node --';
+var ENTRY_POINT = SRC_PATH + '/app.js';
 
-gulp.task('build', function (done) {
-  gulp.src(SRC_FILES)
-    .pipe(sourcemaps.init())
-    .pipe(babel({ optional: ['runtime'] }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(COMPILED_SRC_DIR))
-    .on('finish', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(SRC_FILES, ['build']);
-});
-
-gulp.task('dev', ['watch'], function () {
+gulp.task('dev', function () {
   return nodemon({
-    watch: [COMPILED_SRC_DIR],
-    script: COMPILED_SRC_DIR + ENTRY_PATH
+    watch: ['src'],
+    exec: [RUN_COMMAND, ENTRY_POINT].join(' '),
+    legacyWatch: true
   });
 });
 
 gulp.task('server', function() {
-  new forever.Monitor(COMPILED_SRC_DIR + ENTRY_PATH).start();
+  var child = new (forever.Monitor)(ENTRY_POINT, {
+    command: RUN_COMMAND
+  });
+  child.start();
 });
 
-gulp.task('default', ['build'], function(done) {
+gulp.task('default', function(done) {
   var environment = process.env.NODE_ENV || 'development';
 
   gutil.log('Running tasks for environment \'' + gutil.colors.cyan(environment) + '\'.');
