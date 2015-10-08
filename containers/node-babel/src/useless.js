a551926a7a07f3ccdf9f05d46740f5f4a19c88e3
jmyrland <j@peanuts.no>
2015-10-08 11:04:17 +0200

    Cleanup

--- containers/node-babel/src/useless.js
+++ containers/node-babel/src/useless.js
@@ -2,30 +2,15 @@ import util from 'util';
 
 export default {
   generateMessage: (url, name = 'world!') => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
 
       if (url === '/') {
         const message = util.format('Hello, %s!', name);
         resolve(message);
       }
 
-      reject('nope');
-
       // Unknown url
-      // throw Error('Unknown url.');
+      throw Error('Unknown url.');
     });
   },
 };
-
-// export default function generateMessage(name = 'world', url = '/') {
-//   return new Promise((resolve) => {
-//
-//     if (url === '/') {
-//       const message = util.format('Hello, %s!', name);
-//       resolve(message);
-//     }
-//
-//     // Unknown url
-//     throw Error('Unknown url.');
-//   });
-// }
